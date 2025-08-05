ca4a4f807fff535f69bcb7cc5365812437018ca4
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2025-08-05 11:02:34 +0530

    schedule dto added

---
+++ spring_boot_backend_template/src/main/java/com/etms/dtos/AddScheduleDTO.java
@@ -0,0 +1,38 @@
+package com.etms.dtos;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.etms.pojos.SchduledGroup;
+import com.etms.pojos.ScheduledVenue;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddScheduleDTO {
+	
+	private Long course;
+	
+	private Long modules; //(Foreign Key)
+	@Schema(example = "2025-02-23") 
+	private LocalDate date;
+
+	@Schema(example = "09:30:00") // Valid time format
+    private LocalTime start_time;
+
+    @Schema(example = "11:00:00")
+    private LocalTime end_time;
+
+	
+	private String  type ;//(Lecture/Lab)
+	
+	private SchduledGroup scduledgroup;; //(Theory/E1/E2)
+	
+	private Long faculty_id ;//(Foreign Key to Staff table)
+	
+	private ScheduledVenue scheduledvenue;
+
+}
---
+++ spring_boot_backend_template/src/main/java/com/etms/dtos/ScheduleRespDTO.java
@@ -0,0 +1,34 @@
+package com.etms.dtos;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScheduleRespDTO {
+
+private Long course;
+	
+	private Long modules; //(Foreign Key)
+	@Schema(example = "2025-02-23") 
+	private LocalDate date;
+
+	@Schema(example = "09:30:00") // Valid time format
+    private LocalTime start_time;
+
+    @Schema(example = "11:00:00")
+    private LocalTime end_time;
+
+	
+	private String  type ;//(Lecture/Lab)
+	
+	private String scduledgroup; //(Theory/E1/E2)
+	
+	private Long faculty_id ;//(Foreign Key to Staff table)
+	
+	private String scheduledvenue;
+}
