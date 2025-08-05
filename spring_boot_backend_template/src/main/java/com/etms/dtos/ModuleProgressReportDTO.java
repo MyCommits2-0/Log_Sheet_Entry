1ed5d100b9c2dd7bd94239b50e4c820c746b722a
NileshPawar1996 <pawarsnilesh.1996@gmail.com>
2025-08-05 11:03:41 +0530

    module progress and report dtos added

---
+++ spring_boot_backend_template/src/main/java/com/etms/dtos/ModuleProgressReportDTO.java
@@ -0,0 +1,37 @@
+package com.etms.dtos;
+
+import com.etms.pojos.Courses;
+import com.etms.pojos.Employee;
+import com.etms.pojos.LogType;
+import com.etms.pojos.Logsheet;
+import com.etms.pojos.Modules;
+import com.etms.pojos.Schedules;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ModuleProgressReportDTO {
+    private List<Logsheet> logs;
+    private int topicsCovered;
+    private int totalTopics;
+    private double progressPercentage;
+
+    public ModuleProgressReportDTO(List<Logsheet> logs, int topicsCovered, int totalTopics, double progressPercentage) {
+        this.logs = logs;
+        this.topicsCovered = topicsCovered;
+        this.totalTopics = totalTopics;
+        this.progressPercentage = progressPercentage;
+    }
+
+    // Getters and Setters
+}
---
+++ spring_boot_backend_template/src/main/java/com/etms/dtos/ReportRequestDTO.java
@@ -0,0 +1,16 @@
+package com.etms.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReportRequestDTO {
+    private String course;
+    private String module;
+    private String faculty;
+
+    // Getters and Setters
+}
